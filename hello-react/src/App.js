425e6a1ba3eba14ee55979e2dfee8f37644bc6f1
Sergio Valadez <sergioval@gmail.com>
2022-04-19 16:30:20 -0500

    Set states to the react components

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+        state = {
             username: '',
             password: '',
             passwordConfirmation: '',
             email: '',
             errors: []
         };
-    }
+        validateUsernameOnBlur = this.validateUsernameOnBlur.bind(this);
+
+
     displayForm() {
         return (
             <div>
-                Username: <input type="text" /><br />
+                Username: <input type="text" onBlur={ this.validateUsernameOnBlur }/><br />
                 Password: <input type="text" /><br />
                 Password Confirmation: <input type="text" /><br />
                 Email: <input type="text" /><br />
                 <br />
-                <button>Submit</button>
+                <button onClick={ this.submitForm }>Submit</button>
             </div>
+
         );
+    }
+
+    validateUsernameOnBlur(event) {
+        console.log("I should validate whatever is in ", event.target.value);
+        this.setState();
+    }
+
+    submitForm(event) {
+        console.log("Submitting the form now...");
+        console.log(event);
     }
     render() {
         return (
